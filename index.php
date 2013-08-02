d92040b0f989f0e1a155ab865bdf22a44b470179
Dan Audne <dan.audne@gmail.com>
2013-08-02 16:48:19 +0200

    lots of icons and and some changes

--- index.php
+++ index.php
@@ -3,7 +3,22 @@
 // $items[0] = array(array(0,0,0,0,0,0,0,0,0), array(0,0,0,0,0,0,0,0,0));
 // $items[2] = array(array(0,0,0,0,0,0,0,0,0), array(0,0,0,0,0,0,0,0,0));
 // echo json_encode($items);
-for($i = 256; $i <= 421; $i++)
+// for($i = 256; $i <= 421; $i++)
+// {
+// 	echo '["'.$i.'", ""],<br>';
+// }
+$site = file_get_contents('http://www.minecraftwiki.net/wiki/Data_Values');
+
+preg_match_all('/src="(.*?)" width="25"/i', $site, $out);
+
+$big = array();
+foreach ($out[1] as $v) {
+	$v = str_replace('thumb/', '', $v);
+	$big[] = preg_replace('/\/25px(.*?).png$/i', '', $v);
+}
+$i = 1;
+foreach ($big as $b)
 {
-	echo '["'.$i.'", ""],<br>';
+	file_put_contents("images_minecraft/".$i.".png", file_get_contents($b));
+	$i++;
 }
